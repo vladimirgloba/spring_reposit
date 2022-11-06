0f61cf67c5ebf485c93800152e2bb85c753440a5
vladimirgloba <globavv73@yandex.ru>
2022-11-06 14:20:18 +0300

    06.11.22

--- mod2_lesson6/src/main/java/org/example/web/dto/BookIdToRemove.java
+++ mod2_lesson6/src/main/java/org/example/web/dto/BookIdToRemove.java
@@ -3,14 +3,13 @@ package org.example.web.dto;
 import org.example.app.services.BookExists;
 
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 
 public class BookIdToRemove {
 
 
-    @NotNull (message = "the field is empty")
+    @NotNull(message = "the field is empty")
     @DecimalMin(value = "1.0", message = "invalid number < 0")
     @BookExists(message = "book not found")
     private Integer id;
